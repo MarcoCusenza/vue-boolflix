c1a7d4fef0dfdda144dc76b2476ef3b81ce12b36
Marco Cusenza <marco.cusenza.me@gmail.com>
2021-12-20 16:54:54 +0100

    struttura con shared data

---
+++ src/components/commons/Searchbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="searchbar-container">
+    <h2>Cerca un film</h2>
+    <div class="search-area">
+      <input type="text" name="searchbar" id="searchbar" v-model="dataShared.selectVal"/>
+      <button>Cerca</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import dataShared from "../../share/dataShared";
+
+export default {
+  name: "Searchbar",
+  data() {
+    return {
+      dataShared,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/partials/variables.scss";
+
+.searchbar-container {
+  font-size: $bigger-text;
+  padding: $small-space;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .search-area * {
+    margin: $small-space;
+  }
+}
+</style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,12 +1,25 @@
 <template>
-  <main></main>
+  <main>
+    <Searchbar />
+  </main>
 </template>
 
 <script>
+import Searchbar from "../commons/Searchbar.vue";
+
 export default {
   name: "Main",
+  components: {
+    Searchbar,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/style/partials/variables.scss";
+
+main {
+  background-color: $darker;
+  color: $light-text;
+}
 </style>
